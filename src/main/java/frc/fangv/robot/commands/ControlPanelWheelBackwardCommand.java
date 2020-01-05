60cac318b9c5f0ed5a11bec66ca5a7c088b45f5c
grant <grantday10@gmail.com>
2020-01-04 22:02:24 -0500

    Control Panel Wheel Backward/Forward/Stop Commands

---
+++ src/main/java/frc/fangv/robot/commands/ControlPanelWheelBackwardCommand.java
@@ -0,0 +1,36 @@
+package frc.fangv.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ControlPanelWheelBackwardCommand extends Command {
+
+    public ControlPanelWheelBackwardCommand() {
+        // If any subsystems are needed, you will need to pass them into the requires() method
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void execute() {
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        // TODO: Make this return true when this Command no longer needs to run execute()
+        return false;
+    }
+
+    @Override
+    protected void end() {
+
+    }
+
+    @Override
+    protected void interrupted() {
+
+    }
+}
